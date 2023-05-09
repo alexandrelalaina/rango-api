162ce9817a14ca272d7c355751fe308e76c3d0a4
Alexandre La Laina <alexandre_lalaina@yahoo.com.br>
2023-05-09 15:06:51 -0300

    230509 criacao do getItemListByReceitaId

--- src/main/java/com/rango/domain/model/Item.java
+++ src/main/java/com/rango/domain/model/Item.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
@@ -14,9 +15,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Entity
 @Table(name = "item")
-public class Item {
-
-//    private static final long serialVersionUID = 1L;
+public class Item implements Serializable {
 
     @Id
     @SequenceGenerator(name = "seq_item", sequenceName = "seq_item")
